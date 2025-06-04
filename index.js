f7ccc1dd823c550b04b5ab38c10d01effdc273b2
abhayjt <ad.dubey23@gmail.com>
2025-06-04 13:03:50 +0530

    Add the  indejs  file bug fix

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-import express  from "express"
- import path   from "path";
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-   const app=express();
+const app = express();
 
-    app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-
-        // res.send("Hi! This server is running now. Success Wit jenkin  fiast deplaymet ");
-
-        res.sendFile(path.join(__dirname, 'index.html'));
-    })
-
-
-  app.listen(3000, () => {
+app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
